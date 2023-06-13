138a759f9aef4de520444c0725f8565283b3a725
Ryoto <c0b22054b1@edu.teu.ac.jp>
2023-06-13 17:13:54 +0900

    演習1終了

--- pygame_image.py
+++ pygame_image.py
@@ -9,16 +9,22 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     img=pg.image.load("ex01/fig/3.png")
     img=pg.transform.flip(img,True,False)
-    imgs=[img,pg.transform.rotozoom(img,10,1.0)]
+    imgs=[img,pg.transform.rotozoom(img,2,1.0),pg.transform.rotozoom(img,4,1.0),
+          pg.transform.rotozoom(img,6,1.0),pg.transform.rotozoom(img,8,1.0),
+          pg.transform.rotozoom(img,10,1.0),pg.transform.rotozoom(img,8,1.0),
+          pg.transform.rotozoom(img,6,1.0),pg.transform.rotozoom(img,4,1.0)
+          ,pg.transform.rotozoom(img,2,1.0)]
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x=tmr%1600
-        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [-x,0])
         screen.blit(bg_img,[1600-x,0])
-        screen.blit(imgs[tmr%2],[300,200])
+        screen.blit(imgs[tmr%10],[300,200])
         # screen.blit(bg_img,[1600-x,0])
+        # screen.blit(imgs[tmr%2],[0,y])
         pg.display.update()
         tmr += 1        
         clock.tick(100)
